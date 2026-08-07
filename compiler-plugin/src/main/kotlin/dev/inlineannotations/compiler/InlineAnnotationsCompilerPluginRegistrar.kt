c5ca3f4896605e672b8eea0a66a74bf0cfca9734
Seán Mac Gillicuddy <seantheappdev@gmail.com>
2026-08-07 22:43:37 +0100

    register FIR inline annotation expansion

--- compiler-plugin/src/main/kotlin/dev/inlineannotations/compiler/InlineAnnotationsCompilerPluginRegistrar.kt
+++ compiler-plugin/src/main/kotlin/dev/inlineannotations/compiler/InlineAnnotationsCompilerPluginRegistrar.kt
@@ -4,6 +4,7 @@ import org.jetbrains.kotlin.backend.common.extensions.IrGenerationExtension
 import org.jetbrains.kotlin.compiler.plugin.CompilerPluginRegistrar
 import org.jetbrains.kotlin.compiler.plugin.ExperimentalCompilerApi
 import org.jetbrains.kotlin.config.CompilerConfiguration
+import org.jetbrains.kotlin.fir.extensions.FirExtensionRegistrarAdapter
 
 @OptIn(ExperimentalCompilerApi::class)
 public class InlineAnnotationsCompilerPluginRegistrar : CompilerPluginRegistrar() {
@@ -12,6 +13,7 @@ public class InlineAnnotationsCompilerPluginRegistrar : CompilerPluginRegistrar(
     override val supportsK2: Boolean = true
 
     override fun ExtensionStorage.registerExtensions(configuration: CompilerConfiguration) {
+        FirExtensionRegistrarAdapter.registerExtension(InlineAnnotationsFirExtensionRegistrar())
         IrGenerationExtension.registerExtension(InlineAnnotationsIrGenerationExtension())
     }
 }
