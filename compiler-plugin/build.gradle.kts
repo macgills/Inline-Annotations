3eb7dc64631520ba755f63d99d1d87e3c129c15d
Seán Mac Gillicuddy <seantheappdev@gmail.com>
2026-08-07 22:38:27 +0100

    prove compiler plugin service discovery

--- compiler-plugin/build.gradle.kts
+++ compiler-plugin/build.gradle.kts
@@ -8,6 +8,10 @@ kotlin {
 
 dependencies {
     compileOnly("org.jetbrains.kotlin:kotlin-compiler-embeddable:2.4.10")
+
+    testImplementation(kotlin("test-junit"))
+    testImplementation("junit:junit:4.13.2")
+    testImplementation("org.jetbrains.kotlin:kotlin-compiler-embeddable:2.4.10")
 }
 
 tasks.jar {
