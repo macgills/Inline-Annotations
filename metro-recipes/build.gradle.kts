8ce4ec80149775f7061b0f3103684085e008f6ea
Seán Mac Gillicuddy <seantheappdev@gmail.com>
2026-08-08 04:02:22 +0100

    add Metro inline annotation recipe producer

---
+++ metro-recipes/build.gradle.kts
@@ -0,0 +1,30 @@
+import org.gradle.jvm.tasks.Jar
+import org.jetbrains.kotlin.gradle.tasks.KotlinJvmCompile
+
+plugins {
+    kotlin("jvm")
+}
+
+kotlin {
+    jvmToolchain(21)
+}
+
+dependencies {
+    implementation(project(":annotations"))
+    implementation("dev.zacsweers.metro:runtime:1.3.2")
+}
+
+evaluationDependsOn(":compiler-plugin")
+val inlineAnnotationsCompilerPluginJar = project(":compiler-plugin").tasks.named<Jar>("jar")
+
+tasks.withType<KotlinJvmCompile>().configureEach {
+    dependsOn(inlineAnnotationsCompilerPluginJar)
+    pluginClasspath.from(inlineAnnotationsCompilerPluginJar.flatMap { it.archiveFile })
+
+    doFirst {
+        val pluginFiles = pluginClasspath.files
+        check(pluginFiles.any { it.name == "inline-annotations-compiler-plugin.jar" }) {
+            "Inline annotations compiler plugin missing from $name pluginClasspath: $pluginFiles"
+        }
+    }
+}
