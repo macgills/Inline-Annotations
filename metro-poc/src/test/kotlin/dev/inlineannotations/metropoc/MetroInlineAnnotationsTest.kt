4e7f765894052628e26528590c700c4f5be43d97
Seán Mac Gillicuddy <seantheappdev@gmail.com>
2026-08-08 04:46:31 +0100

    verify explicit Metro scoped bindings

--- metro-poc/src/test/kotlin/dev/inlineannotations/metropoc/MetroInlineAnnotationsTest.kt
+++ metro-poc/src/test/kotlin/dev/inlineannotations/metropoc/MetroInlineAnnotationsTest.kt
@@ -1,6 +1,6 @@
 package dev.inlineannotations.metropoc
 
-import dev.inlineannotations.metrorecipes.AppScopedProvider
+import dev.inlineannotations.metrorecipes.AppScopedBinding
 import kotlin.test.Test
 import kotlin.test.assertEquals
 import kotlin.test.assertNull
@@ -8,7 +8,7 @@ import kotlin.test.assertSame
 
 public class MetroInlineAnnotationsTest {
     @Test
-    public fun metroBuildsARealGraphFromInlineScopedProviders() {
+    public fun metroBuildsARealGraphFromInlineScopedBindings() {
         val graph = createAppGraph()
 
         assertEquals(
@@ -20,15 +20,7 @@ public class MetroInlineAnnotationsTest {
         assertSame(graph.userRepository, graph.userRepository)
         assertSame(graph.analytics, graph.analytics)
 
-        assertNull(
-            AppGraph::class.java
-                .getDeclaredMethod("provideUserRepository")
-                .getAnnotation(AppScopedProvider::class.java),
-        )
-        assertNull(
-            AppGraph::class.java
-                .getDeclaredMethod("provideAnalytics", UserRepository::class.java)
-                .getAnnotation(AppScopedProvider::class.java),
-        )
+        assertNull(RealUserRepository::class.java.getAnnotation(AppScopedBinding::class.java))
+        assertNull(DefaultAnalytics::class.java.getAnnotation(AppScopedBinding::class.java))
     }
 }
