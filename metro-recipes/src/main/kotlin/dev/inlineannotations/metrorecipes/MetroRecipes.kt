c5285e17745bc6bb32f59e247a43962cb2d9d72c
Seán Mac Gillicuddy <seantheappdev@gmail.com>
2026-08-08 04:32:55 +0100

    focus Metro recipe on repeated scoped bindings

--- metro-recipes/src/main/kotlin/dev/inlineannotations/metrorecipes/MetroRecipes.kt
+++ metro-recipes/src/main/kotlin/dev/inlineannotations/metrorecipes/MetroRecipes.kt
@@ -3,30 +3,25 @@
 package dev.inlineannotations.metrorecipes
 
 import dev.inlineannotations.InlineAnnotations
-import dev.zacsweers.metro.BindingContainer
 import dev.zacsweers.metro.ContributesBinding
-import dev.zacsweers.metro.ContributesTo
-import dev.zacsweers.metro.DependencyGraph
 import dev.zacsweers.metro.SingleIn
 
+/** Application lifetime used by the Metro proof-of-concept graph. */
 public object AppScope
 
-@InlineAnnotations // prototype-only binary recipe marker for downstream compilation
-@DependencyGraph(AppScope::class)
-@Target(AnnotationTarget.CLASS)
-@Retention(AnnotationRetention.RUNTIME)
-public inline annotation class AppGraph
-
+/**
+ * The repetitive Metro pattern this proof is intended to remove.
+ *
+ * Without inline annotation classes every app-scoped implementation repeats both annotations and
+ * the same scope argument:
+ *
+ * @ContributesBinding(AppScope::class)
+ * @SingleIn(AppScope::class)
+ * class RealThing : Thing
+ */
 @InlineAnnotations // prototype-only binary recipe marker for downstream compilation
 @ContributesBinding(AppScope::class)
 @SingleIn(AppScope::class)
 @Target(AnnotationTarget.CLASS)
 @Retention(AnnotationRetention.RUNTIME)
-public inline annotation class AppSingletonBinding
-
-@InlineAnnotations // prototype-only binary recipe marker for downstream compilation
-@ContributesTo(AppScope::class)
-@BindingContainer
-@Target(AnnotationTarget.CLASS)
-@Retention(AnnotationRetention.RUNTIME)
-public inline annotation class AppBindingContainer
+public inline annotation class AppScopedBinding
